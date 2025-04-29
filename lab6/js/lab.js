edb4292f07d88960ff9ec2f6a47ce003681baba6
audrey-de-merch <ademerch@ucsc.edu>
2025-04-28 22:10:44 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
 
-// Constants
+// Author: Audrey DeMerchant
+// Date: April 28th, 2025 
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+
+// Define variables 
+
+myTransport = ["Car", " e-bike", " walking", " the bus", " rides from friends", " Uber"];
+  
+var myMainRide = {
+  make: "Acura",
+  model:"TSX",
+  color: "Black",
+  year: 2014,
+  age: function(){
+    return 2025 - this.year; 
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+//output 
 
-// let's get this party started
-main();
+document.writeln("When I am in Santa Cruz for School I get around mostly by" + myTransport[2]+ " and " + myTransport[3] + ". On occassion I will also take" + myTransport[4] + ", " + myTransport[1]+ ", or " + myTransport[5] + ".<br>" ); 
+
+document.writeln("When I am back in San Diego my main ride is: <pre>", 
+  JSON.stringify(myMainRide, null, "\t" ), "</pre>")
