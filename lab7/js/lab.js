faa77f3341b9bd5c3b61c8cf853b061f70abc1c4
audrey-de-merch <ademerch@ucsc.edu>
2025-05-08 13:04:11 -0700

    lab 7 fix

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -3,3 +3,32 @@
  * Date: May 7, 2025 
  * 
 */
+const myButton = document.createElement('button')
+
+myButton.textContent ="Click to Sort Name";
+myButton.id = 'myButton';
+myButton.className = "my-button";
+myButton.addEventListener('click', 
+  function sortUserName() {
+
+  var userName = window.prompt("Please enter your name")
+  console.log("userName =", userName);
+
+  if (userName) {
+    console.log("userName =", userName);
+  // split string to array because you can't sort strings
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+ 
+    // sort the array 
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort)
+    
+    // turn array back into a string 
+    var nameSorted = nameArraySort.join(" ");
+    console.log("nameSorted =", nameSorted); 
+    
+    alert("Name Fixed: " + nameSorted);
+
+}
+  })
