46c55e62892f44778167d68d7771e71f6dc5f3a6
audrey-de-merch <ademerch@ucsc.edu>
2025-05-12 22:53:14 -0700

    lab 9

---
+++ lab9/js/lab.js
@@ -0,0 +1,19 @@
+/** 
+ * Author: Audrey DeMerchant
+ * Date: May 12, 2025 
+ * 
+*/
+// button to challenge section
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+
+// click listener 
+
+$('#button-challenge').click(function(){
+  $("#challenge").toggleClass("special");
+
+
+});
+
+
+
+
